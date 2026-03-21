a8e6610350c4e88f8b06171e21da045eb7a4a219
sankalpsetufoundationse <sankalpsetufoundationserver@gmail.com>
2026-03-21 16:34:20 +0530

    Update user.js

--- model/SQL_Model/user.js
+++ model/SQL_Model/user.js
@@ -3,58 +3,61 @@ const bcrypt = require("bcrypt");
 const sequelize = require("../../config/sqlcon");
 
 const User = sequelize.define(
-"User",
-{
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
+  "User",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
 
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    role_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
+    branch_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "branches",
+        key: "id"
+      }
+    },
+
+    is_active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    },
+
+    last_login: {
+      type: DataTypes.DATE,
+      allowNull: true
     }
   },
-
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-
-  role_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-
-  branch_id: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    references: {
-      model: "branches",
-      key: "id"
-    }
-  },
-
-  is_active: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true
+  {
+    tableName: "users",
+    underscored: true,
+    timestamps: true
   }
-},
-{
-  tableName: "users",
-  underscored: true,
-  timestamps: true 
-}
 );
-
 
 User.beforeCreate(async (user) => {
   user.password = await bcrypt.hash(user.password, 10);
 });
-
 
 User.prototype.validatePassword = function (password) {
   return bcrypt.compare(password, this.password);
