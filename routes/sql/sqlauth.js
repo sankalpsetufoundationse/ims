557a48f82ec5d923902f2c8f220771d2597daccc
sankalpsetufoundationse <sankalpsetufoundationserver@gmail.com>
2026-03-27 12:10:16 +0530

    Update sqlauth.js

--- routes/sql/sqlauth.js
+++ routes/sql/sqlauth.js
@@ -15,14 +15,14 @@ router.post("/verify-password-reset-otp", authController.verifyPasswordResetOTP)
 router.post("/reset-password", authController.resetPasswordWithOTP);
 
 
-router.get("/notifications", auth,checkRole(["super_admin"]), authController.getMyNotifications);
+router.get("/notifications", auth,checkRole(["super_admin","admin"]), authController.getMyNotifications);
 router.patch(
   "/notifications/:id/read",
- auth,checkRole(["super_admin"]),
+ auth,checkRole(["super_admin","admin"]),
   authController.markNotificationRead
 );
 
 
-router.get("/recent-activities",auth,checkRole(["super_admin"]), authController.getRecentActivities);
+router.get("/recent-activities",auth,checkRole(["super_admin","admin"]), authController.getRecentActivities);
 
 module.exports = router;
