3c962b79aac8861a8e16c339f5689802309114b7
sankalpsetufoundationse <sankalpsetufoundationserver@gmail.com>
2026-03-28 16:14:52 +0530

    Update csv.js

--- routes/sql/csv.js
+++ routes/sql/csv.js
@@ -8,6 +8,10 @@ const  exportcsv  = require("../../controllers/sqlbase/csvdownload");
 
 router.get("/stock-aging/export", auth,exportcsv.exportStockAgingExcel);
 router.get("/inventory/export-excel",auth,exportcsv.exportInventoryExcel);
-router.get("/report/export-excel",auth,exportcsv.getReportsAnalyticsDashboard);
+router.get("/report/export-excel",auth,exportcsv.exportSalesExcel);
 router.get("/ladger/export-excel",auth,exportcsv.exportClientsExcel);
+router.get('/user-get/export-excel',auth,exportcsv.exportUsersExcel);
+router.get('/report-superadmin/export-excel',auth,exportcsv.downloadReport)
+// router.get('/ladger-client',auth,exportcsv.exportSalesExcel)
+// router.get('/')
 module.exports = router;
